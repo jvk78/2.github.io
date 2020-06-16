d765f7237f11a5620d37e79a64e9cc681d2363c1
jvk <60311804+jvk78@users.noreply.github.com>
2020-06-16 12:31:49 +0600

    Update base_img.js

--- assets/app/base_img.js
+++ assets/app/base_img.js
@@ -10,43 +10,6 @@ const prodLeft = [
     url: './assets/img/products/vermishel.png',
     name:'Вермишель',
     alt: 'vermishel'
-  },
-  {
-    id: 'left_3',
-    url: './assets/img/products/vitki.png',
-    name:'Витки',
-    alt: 'vitki'
-  },
-  {
-    id: 'left_4',
-    url: './assets/img/products/perya.png',
-    name:'Перья',
-    alt: 'perya'
-  },
-  {
-    id: 'left_5',
-    url: './assets/img/products/rakushki.png',
-    name:'Ракушки',
-    alt: 'rakushki'
-  },
-  {
-    id: 'left_6',
-    url: './assets/img/products/rozhki.png',
-    name:'Рожки',
-    alt: 'rozhki'
-  },
-  {
-    id: 'left_7',
-    url: './assets/img/products/spagetti.png',
-    name:'Спагетти',
-    alt: 'spagetti'
-  },
-  
-  {
-    id: 'left_8',
-    url: './assets/img/products/spiral.png',
-    name:'Спирали',
-    alt: 'spirali'
   }
   
 ]
@@ -63,24 +26,6 @@ const prodRight = [
     url: './assets/img/products/egg_rozhki.png',
     name:'Рожки',
     alt: 'egg_rozhki'
-  },
-  {
-    id: 'right_3',
-    url: './assets/img/products/egg_spagetti.png',
-    name:'Спагетти',
-    alt: 'egg_spagetti'
-  },
-  {
-    id: 'right_4',
-    url: './assets/img/products/egg_spiral.png',
-    name:'Спирали',
-    alt: 'egg_spiral'
-  },
-  {
-    id: 'right_5',
-    url: './assets/img/products/egg_vermishel.png',
-    name:'Вермишель',
-    alt: 'egg_vermishel'
   }
   
 ]
